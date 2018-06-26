30f1135500ffe2def0f6026bd591a00c75a85690
Павел Петриковский <petrikovskiy@lanit.ru>
2018-06-26 09:03:30 +0500

    Рабочая версия

--- googbot.py
+++ googbot.py
@@ -6,22 +6,33 @@ from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
+API_NAME = 'sheets'
+API_VERSION = 'v4'
+API_CREDANTIALS = 'credentials.json'
+SHEET_ADDRES = 'sheet_address.json'
+
 class Googbot:
 	def __init__(self):
 		# Setup the Sheets API
-		self.store = file.Storage('credentials.json')
+		try:
+			self.store = file.Storage(API_CREDANTIALS)
+		except Exception:
+			raise Exception('Credentials file not found...')
 		self.creds = self.store.get()
 		if not self.creds or self.creds.invalid:
-		    raise Exception(f'Credentials file not found...')
-		self.service = build('sheets', 'v4', http=self.creds.authorize(Http()))
+		    raise Exception('Credentials not found...')
+		self.service = build(API_NAME, API_VERSION, http=self.creds.authorize(Http()))
 
 		# Call the Sheets API
-		with open('sheet_address.json', 'r') as f:
-			sheet_address = json.load(f)
+		try:
+			with open(SHEET_ADDRES, 'r') as f:
+				sheet_address = json.load(f)
+		except Exception:
+			raise Exception('Addres file not found...')
 		self.SPREADSHEET_ID = sheet_address['sheet_id']
 		self.RANGE_NAME = sheet_address['list_name'] + '!{}'
 		
-	def get_data_from_sheet(self, my_sheet_range) -> list:
+	def post_data_from_sheet(self, my_sheet_range) -> list:
 		result = self.service.spreadsheets().values().get(
 			spreadsheetId=self.SPREADSHEET_ID,
 			range=self.RANGE_NAME.format(my_sheet_range)).execute()
--- main.py
+++ main.py
@@ -1,20 +1,65 @@
 from googbot import Googbot
 
 
-ATTENDANT_NUMBERS = 'A18:B23'
-CHEL_SHIFT_TIME = 'E18:E23'
-MSK_SHIFT_TIME = 'F18:F23'
+class ShiftSheet:
+	def __init__(self):
+		self.MONTHS = ['yanuar','februar','march','april','may','juny',
+			'july','august','september','october','november','december']
+		self.ATTENDANT_NUMBERS_FIELD = 'A18:B23'
+		self.CHEL_SHIFT_TIME_FIELD = 'E18:E23'
+		self.MSK_SHIFT_TIME_FIELD = 'F18:F23'
 
-OFFSET = 4
-MONTHS = ['jiny','july','august','september','october','november',
-		  'december','yanuar','februar','march','april','may']
-MONTH_OFFSET = 4
-DAY_OF_MONTH = 'G17:AK17'
-WORK_FILD = 'G18:AK23'
+		#Sheet's fields offsets
+		self.OFFSET_START_FROM = 16
+		self.WORK_FIELD_START_OFFSET = 2
+		self.WORK_FIELD_END_OFFSET = 8
+		self.DATE_FIELD_OFFSET = 1
+		self.NEXT_BLOCK_OFFSET = 9
 
-attendant_numbers = Googbot().get_data_from_sheet(ATTENDANT_NUMBERS)
-day_of_month = Googbot().get_data_from_sheet(DAY_OF_MONTH)
-work_fild = Googbot().get_data_from_sheet(WORK_FILD)
-print(attendant_numbers)
-print(day_of_month)
-print(work_fild)
+		#Date offset
+		self.MONTH_OFFSET = 5
+
+		self.FIELD_TEMTLATE = 'G{}:AK{}' #'G18:AK23' WORK_FIELD
+		self.MONTH_NAME_TEMPLATE = 'A{}:A{}'
+
+		self.googbot = Googbot()
+
+	def __month_on_sheet(self, actual_month) -> int:
+		month_on_sheet = self.MONTHS.index(actual_month)-self.MONTH_OFFSET
+		if month_on_sheet < 0:
+			month_on_sheet += 12
+		return month_on_sheet
+
+	def __block_number(self) -> int:
+		return self.__month_on_sheet()*self.NEXT_BLOCK_OFFSET
+
+	def __month_offset(self, actual_month) -> int:
+		return (
+			self.OFFSET_START_FROM+ \
+			self.__month_on_sheet(actual_month)* \
+			self.NEXT_BLOCK_OFFSET)
+
+	def post_month_name(self, actual_month) -> list:
+		month_coordinates = self.__month_offset(actual_month)
+		field = self.MONTH_NAME_TEMPLATE.format(month_coordinates,month_coordinates)
+		return self.googbot.post_data_from_sheet(field)
+
+	def post_date_field(self, actual_month) -> list:
+		date_coordinates = self.DATE_FIELD_OFFSET+self.__month_offset(actual_month)
+		field = self.FIELD_TEMTLATE.format(date_coordinates,date_coordinates)
+		return self.googbot.post_data_from_sheet(field)
+
+	def post_work_field(self, actual_month) -> list:
+		work_field_coordinate_1 = (
+			self.WORK_FIELD_START_OFFSET+self.__month_offset(actual_month))
+		work_field_coordinate_2 = (
+			self.WORK_FIELD_END_OFFSET+self.__month_offset(actual_month))
+		field = self.FIELD_TEMTLATE.format(
+			work_field_coordinate_1,work_field_coordinate_2)
+		return self.googbot.post_data_from_sheet(field)
+
+
+ss = ShiftSheet()
+print(ss.post_month_name('yanuar'))
+print(ss.post_date_field('yanuar'))
+print(ss.post_work_field('yanuar'))
