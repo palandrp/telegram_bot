ed84004b780ba4b0a5a67dc48bbada3a90e826f4
Павел Петриковский <petrikovskiy@lanit.ru>
2018-06-26 12:22:43 +0500

    Остановился на том что создаются пары время:'номер дежурного'

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from googbot import Googbot
 
 class ShiftSheet:
 	def __init__(self):
-		self.MONTHS = ['yanuar','februar','march','april','may','juny',
+		self.MONTHS = [
+			'yanuar','februar','march','april','may','juny',
 			'july','august','september','october','november','december']
+		self.X_COORDINATES = [
+			'G','H','I','J','K','L','M','N','O','P','Q',
+			'R','S','T','U','V','W','X','Y','Z','AA','AB',
+			'AC','AD','AE','AF','AG','AH','AI','AJ','AK']
 		self.ATTENDANT_NUMBERS_FIELD = 'A18:B23'
 		self.CHEL_SHIFT_TIME_FIELD = 'E18:E23'
 		self.MSK_SHIFT_TIME_FIELD = 'F18:F23'
@@ -15,12 +20,13 @@ class ShiftSheet:
 		self.WORK_FIELD_END_OFFSET = 8
 		self.DATE_FIELD_OFFSET = 1
 		self.NEXT_BLOCK_OFFSET = 9
+		self.DAY_OFFSET = 1
 
 		#Date offset
 		self.MONTH_OFFSET = 5
 
-		self.FIELD_TEMTLATE = 'G{}:AK{}' #'G18:AK23' WORK_FIELD
-		self.MONTH_NAME_TEMPLATE = 'A{}:A{}'
+		self.FIELD_TEMTLATE = 'G{}:AK{}'
+		self.MONTH_NAME_TEMPLATE = 'A{}'
 
 		self.googbot = Googbot()
 
@@ -39,27 +45,73 @@ class ShiftSheet:
 			self.__month_on_sheet(actual_month)* \
 			self.NEXT_BLOCK_OFFSET)
 
-	def post_month_name(self, actual_month) -> list:
-		month_coordinates = self.__month_offset(actual_month)
-		field = self.MONTH_NAME_TEMPLATE.format(month_coordinates,month_coordinates)
-		return self.googbot.post_data_from_sheet(field)
-
-	def post_date_field(self, actual_month) -> list:
-		date_coordinates = self.DATE_FIELD_OFFSET+self.__month_offset(actual_month)
-		field = self.FIELD_TEMTLATE.format(date_coordinates,date_coordinates)
-		return self.googbot.post_data_from_sheet(field)
-
-	def post_work_field(self, actual_month) -> list:
+	def __month_field_Y_coordinates(self, actual_month) -> list:
 		work_field_coordinate_1 = (
 			self.WORK_FIELD_START_OFFSET+self.__month_offset(actual_month))
 		work_field_coordinate_2 = (
 			self.WORK_FIELD_END_OFFSET+self.__month_offset(actual_month))
-		field = self.FIELD_TEMTLATE.format(
-			work_field_coordinate_1,work_field_coordinate_2)
+		return [work_field_coordinate_1,work_field_coordinate_2]
+
+	def __X_coordinate(self, actual_day) -> str:
+		return self.X_COORDINATES[int(actual_day)-self.DAY_OFFSET]
+
+	def post_month_name(self, actual_month) -> str:
+		month_coordinate = self.__month_offset(actual_month)
+		field = self.MONTH_NAME_TEMPLATE.format(month_coordinate)
+		return self.googbot.post_data_from_sheet(field)[0][0]
+
+	def post_date_field(self, actual_month) -> list:
+		date_coordinates = self.DATE_FIELD_OFFSET+self.__month_offset(actual_month)
+		field = self.FIELD_TEMTLATE.format(date_coordinates,date_coordinates)
+		return self.googbot.post_data_from_sheet(field)[0]
+
+	def post_month_field(self, actual_month) -> list:
+		coordinates_Y = self.__month_field_Y_coordinates(actual_month)
+		field = self.FIELD_TEMTLATE.format(coordinates_Y[0],coordinates_Y[1])
 		return self.googbot.post_data_from_sheet(field)
+
+	def calc_shift_date(self, actual_month, actual_day) -> list:
+		return self.post_date_field(actual_month)[int(actual_day)-self.DAY_OFFSET]
+
+	def post_shift_field(self, actual_month, actual_day) -> list:
+		coordinates_Y = self.__month_field_Y_coordinates(actual_month)
+		coordinate_X = self.__X_coordinate(actual_day)
+		field = f'{coordinate_X}{coordinates_Y[0]}:{coordinate_X}{coordinates_Y[1]}'
+		return self.googbot.post_data_from_sheet(field)
+
+	def post_time_intervals(self, location='MSK') -> list:
+		if (location == 'MSK'): field = self.MSK_SHIFT_TIME_FIELD
+		elif (location == 'CHEL'): field = self.CHEL_SHIFT_TIME_FIELD
+		else: raise Exception('Location not found...')
+		temp = self.googbot.post_data_from_sheet(field)
+		return [temp[i][0] for i in range(len(temp))]
+
+	def calc_shift_pairs(self, actual_month, actual_day, location) -> dict:
+		shift_field = self.post_shift_field(actual_month, actual_day)
+		time_intervals = self.post_time_intervals(location)
+		shift_pairs = {}
+		for k, v in zip(time_intervals,shift_field):
+			if k in shift_pairs:
+				shift_pairs[k] += v
+			else:
+				shift_pairs[k] = v
+		return shift_pairs # dict(zip(time_intervals,shift_field))
+
 
 
 ss = ShiftSheet()
-print(ss.post_month_name('yanuar'))
-print(ss.post_date_field('yanuar'))
-print(ss.post_work_field('yanuar'))
+month = ss.post_month_name('july')
+date_list = ss.post_date_field('july')
+shift_list = ss.post_month_field('july')
+actual_shift = ss.post_shift_field('july','01')
+date = ss.calc_shift_date('july','01')
+time_intervals = ss.post_time_intervals('CHEL')
+shift_pairs = ss.calc_shift_pairs('july','01','CHEL')
+
+print(f'Название месяца: {month}')
+print(f'Список дат: {date_list}')
+print(f'Список смен построчно: {shift_list}')
+print(f'Список смен в определённую дату: {actual_shift}')
+print(f'Название даты: {date}')
+print(f'Список временных интервалов: {time_intervals}')
+print(f'Время смены и дежурный: {shift_pairs}')
