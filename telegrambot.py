907eb8538fcd23e3140b079376b3c35c62760b69
Pavel Petrikovskiy <petrikovskiy@lanit.ru>
2019-10-20 16:02:25 +0300

    it's work with proxy. #new_bot

--- telegrambot.py
+++ telegrambot.py
@@ -1,26 +1,37 @@
-import requests
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+import logging
 import myprivat
 
 
 class TelegramBot:
+
     def __init__(self):
         self.TOKEN = myprivat.token
-        self.URL = 'https://api.telegram.org/'
-        self.API_URL = self.URL + 'bot' + self.TOKEN + '/'
+        self.REQUEST_KWARGS={
+            'proxy_url': myprivat.proxy_url,
+            'urllib3_proxy_kwargs': {
+                'username': myprivat.proxy_user,
+                'password': myprivat.proxy_pass,
+            }
+        }
 
-    def get_updates(self):
-        url = self.API_URL + 'getUpdates'
-        r = requests.get(url)
-        return r.json()
+        updater = Updater(token=self.TOKEN, use_context=True,
+                          request_kwargs=self.REQUEST_KWARGS)
+        dispatcher = updater.dispatcher
 
-    def get_message(self):
-        data = self.get_updates()
-        chat_id = data['result'][-1]['message']['chat']['id']
-        message_text = data['result'][-1]['message']['text']
-        message = {'chat_id': chat_id, 'text': message_text}
-        return message
+        logging.basicConfig(
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            level=logging.INFO)
 
-    def send_message(self, chat_id, text='Wait a second, please...'):
-        url = self.API_URL+'sendMessage?'+'chat_id='+str(chat_id)+'&text='+text
-        requests.get(url)
+        def start(update, context):
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text="Test message 1...")
 
+        start_handler = CommandHandler('start', start)
+        dispatcher.add_handler(start_handler)
+
+        updater.start_polling()
+
+
+TelegramBot()
